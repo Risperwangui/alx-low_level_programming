a94af6d9afa9f5256abdeab52815a24dc05993de
Risperwangui <resperwangui68@gmail.com>
2023-03-31 13:42:03 -0700

    Write a code that prints the message given

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * print_message- prints function
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
