f3f1c53cc5ba4b7738f85b9ed77012cc7519db91
Risperwangui <resperwangui68@gmail.com>
2023-04-08 11:18:20 -0700

    Write a function that returns the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - returns the value of a bit
+ * @n: the bit
+ * @index: the index to gwt the value
+ * Return: If an error occurs,-1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	return (1);
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -3,4 +3,5 @@ Question 0:
 Write a function that converts a binary number to an unsigned bit.
 Question 1:
 Wrte a function that prints the binary representation of a number.
-
+Question 2:
+Write a function that returns the value of a bit at a given index.
