be955d9caae22fe3c84c2b8304ad5bd8a77087e3
Risperwangui <resperwangui68@gmail.com>
2023-04-10 06:27:02 -0700

    correcting my code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,25 +11,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, w, r;
-	char *king;
+	char *buffer;
 
-	king = malloc(sizeof(char) * letters);
-		if (king == NULL)
+	buffer = malloc(sizeof(char) * letters);
+		if (buffer == NULL)
 			return (0);
 	o = open(filename, O_RDONLY);
-	w = write(STDOUT_FILENO, king, r);
-	r = read(o, king, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, buffer, letters);
 
 	if (filename == NULL)
 		return (0);
 
 	while (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(king);
+		free(buffer);
 		return (0);
 	}
 
-	free(king);
+	free(buffer);
 	close(o);
 
 	return (w);
