4bff103076a33078117022ee13e17e385374fb28
Risperwangui <resperwangui68@gmail.com>
2023-04-10 07:13:18 -0700

    creating a function that craetes a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/***
+ * create_file - creates a function that creates a file
+ * @filename: a pointer to the filename
+ * @text_content: A pointer to a string
+ * Return: if the function fails, -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int ris, wan, ndu  = 0;
+
+	if (ris == -1 || wan == -1)
+		return (-1);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (ndu = 0; text_content[ndu];)
+			ndu++;
+	}
+
+	ris = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wan = write(ris, text_content, ndu);
+	close(ris);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,5 +2,8 @@ This is a project on File Input/Output
 
 The questions are as follows:
 
-Question 1:
+Question 0:
 Write a file that reads a text file and prints it to the POSIX standard output.
+
+Question 1:
+Create a function that creates a file.
