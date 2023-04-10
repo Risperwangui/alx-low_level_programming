8ca89c522fee84cc8b23ca3d20545f0790c34151
Risperwangui <resperwangui68@gmail.com>
2023-04-10 07:15:44 -0700

    creating a function that craetes a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _putchar(char c);
 #endif
