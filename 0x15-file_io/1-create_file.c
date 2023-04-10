2e61517f9223c9d15091d5960b15d45b3eba1a9d
Risperwangui <resperwangui68@gmail.com>
2023-04-10 07:49:27 -0700

    correcting my code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,14 +17,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	ris = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wan = write(ris, text_content, ndu);
+
 	if (text_content != NULL)
 	{
 		for (ndu = 0; text_content[ndu];)
 			ndu++;
 	}
 
-	ris = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wan = write(ris, text_content, ndu);
 	close(ris);
 
 	return (1);
